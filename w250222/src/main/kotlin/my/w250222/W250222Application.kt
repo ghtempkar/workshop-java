83700e96ed513e93129cd2aaf3169e75d85ffc2e
dk <dk@localhost>
2025-02-22 06:28:32 +0100

    w250222 - init

---
+++ w250222/src/main/kotlin/my/w250222/W250222Application.kt
@@ -0,0 +1,11 @@
+package my.w250222
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W250222Application
+
+fun main(args: Array<String>) {
+	runApplication<W250222Application>(*args)
+}
---
+++ w250222/src/test/kotlin/my/w250222/W250222ApplicationTests.kt
@@ -0,0 +1,13 @@
+package my.w250222
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W250222ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
