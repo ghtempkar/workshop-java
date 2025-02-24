dd82eb3c2d13c2b10e2c6973abd25197267aed50
dk <dk@localhost>
2025-02-24 15:28:38 +0100

    w250224s2

---
+++ w250224s2/src/main/kotlin/my/w250224s2/W250224s2Application.kt
@@ -0,0 +1,11 @@
+package my.w250224s2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W250224s2Application
+
+fun main(args: Array<String>) {
+    runApplication<W250224s2Application>(*args)
+}
