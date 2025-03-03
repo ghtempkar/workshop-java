001e28f6a17bb384e5d544343244710da4197a86
dk <dk@localhost>
2025-03-03 07:27:53 +0100

    w250303s1 init

---
+++ w250303s1/src/main/kotlin/my/w250303s1/W250303s1Application.kt
@@ -0,0 +1,11 @@
+package my.w250303s1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W250303s1Application
+
+fun main(args: Array<String>) {
+	runApplication<W250303s1Application>(*args)
+}
---
+++ w250303s1/src/test/kotlin/my/w250303s1/W250303s1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package my.w250303s1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W250303s1ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
