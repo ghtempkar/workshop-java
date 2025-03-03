730fa983c2fe2d087256e491008c0183ef3d735e
dk <dk@localhost>
2025-03-03 18:57:14 +0100

    w250303s3

--- w250303s3/src/main/kotlin/my/w250303s3/W250303s3Application.kt
+++ w250303s3/src/main/kotlin/my/w250303s3/W250303s3Application.kt
@@ -2,6 +2,16 @@ package my.w250303s3
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class MyController {
+    @GetMapping("/")
+    fun index(): String {
+        return "index"
+    }
+}
 
 @SpringBootApplication
 class W250303s3Application
