7b9f6dbeafe9caf3830f458babc9b7b3618acbe6
dk <dk@localhost>
2025-03-03 09:18:34 +0100

    w250303s1

--- w250303s1/src/test/kotlin/my/w250303s1/sample2/MyTest.kt
+++ w250303s1/src/test/kotlin/my/w250303s1/sample2/MyTest.kt
@@ -2,16 +2,39 @@ package my.w250303s1.sample2
 
 import my.w250303s1.sample2.MyTest.IntTestConfig
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.cache.CacheManager
+import org.springframework.cache.annotation.Cacheable
+import org.springframework.cache.annotation.EnableCaching
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
+import org.springframework.stereotype.Service
+import java.time.Instant
+import kotlin.system.measureTimeMillis
+
+@Service
+internal class MyService {
+    @Cacheable("cache-1")
+    fun myTimestamp1(): String {
+        Thread.sleep(500)
+        return Instant.now().toString()
+    }
+
+    @Cacheable("cache-2")
+    fun myTimestamp2(): String {
+        Thread.sleep(500)
+        return Instant.now().toString()
+    }
+}
 
 @Configuration
+@EnableCaching
+@Import(MyService::class)
 internal class MyConfig {
     @Bean
     fun cacheManager(): CacheManager {
@@ -23,7 +46,9 @@ internal class MyConfig {
 @Import(
     MyConfig::class
 )
-internal class MyTest {
+internal class MyTest(
+    @Autowired val myService: MyService
+) {
 
     @Test
     fun testZero() {
@@ -33,6 +58,28 @@ internal class MyTest {
     fun initTest(@Autowired cacheManager: CacheManager) {
         assertThat(cacheManager.cacheNames).doesNotContain("some-cache")
         assertThat(cacheManager.cacheNames).contains("cache-1")
+    }
+
+    @Test
+    fun `test cache-1`() {
+        measureTimeMillis {
+            println(myService.myTimestamp1())
+        }.let {
+            println(it)
+        }
+
+        measureTimeMillis {
+            println(myService.myTimestamp1())
+        }.let {
+            println(it)
+        }
+    }
+
+    @Test
+    fun `test cache-2 does not exist`() {
+        assertThatThrownBy {
+            myService.myTimestamp2()
+        }.hasMessageContaining("cache-2")
     }
 
     @Configuration
